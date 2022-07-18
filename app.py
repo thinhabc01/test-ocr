1829678ed44b5312bdec946057ceb30f3b8c1289
thinhabc01 <62796914+thinhabc01@users.noreply.github.com>
2022-07-19 01:53:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,7 @@ def to_text(img_list: list) -> dict:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             
         # To text
-        output = pytesseract.image_to_string(img, lang='eng', boxes=False, \
-           config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789').strip()
+        output = pytesseract.image_to_string(img, config="outputbase digits").strip()
 
         # Remove remove non-ASCII characters but leave periods and spaces
         output = output.encode('ascii', errors='ignore').decode() 
